5b491c7d9dc1a3aa7fd6af4d2bc9056c68201963
zhangqiongtao <zhangqt@.com>
2022-10-05 15:18:53 +0800

    three git

--- src/main/java/com/zhang/git/GitTest.java
+++ src/main/java/com/zhang/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你干嘛git!");
         System.out.println("你干嘛git2!");
+        System.out.println("你干嘛git3!");
     }
 }
